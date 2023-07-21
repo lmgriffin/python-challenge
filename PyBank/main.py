14b6934a56d4f13f8555c8768b8d70bba24744f0
lmgriffin <loganmgriffin98@gmail.com>
2023-07-20 18:32:44 -0700

    did some more

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,32 +1,57 @@
+import os
+import csv
 
+cvspath = os.path.join("PyBank", "Resources", "budget_data.csv")
 
 # Read in all the rows (looping through the rows)
 
+greatest_profit = 0
+greatest_profit_month = ""
+least_profit = 999999
+least_profit_month = ""
 
 total_profit_losses = 0
 total_months = 0
 
-sum_profit_loses = 0
-
+sum_profit_losses = 0
 
 
 # Need profit/losses in the for loop
+with open(cvspath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvreader)
+    for row in csvreader:
 
-    profit_loses = row[1]
+        #Calculate total months here:
+        total_months = total_months + 1
 
-    total_profit_losses = total_profit_losses + profit_loses
+        #Calculate the total amount of "Profit/Losses"
+        profit_losses = int(row[1])
 
-    total_months = total_months + 1
+        total_profit_losses = total_profit_losses + int(row[1])
 
-    if total_months > 1:
-        change = profit_loses - last_profit_losses
+        #Changes in "Profit/Losses" over the entire period (get total of changes)
+        if total_months > 1:
+            change = profit_losses - last_profit_losses
+            sum_profit_losses = sum_profit_losses + change
+            if change > greatest_profit:
+                greatest_profit = change
+                greatest_profit_month = row[0]
+            if change < least_profit:
+                least_profit = change
+                least_profit_month = row[0]                                  
+        last_profit_losses = int(row[1])
 
-    last_profit_loses = row[1]
+        
+
 
 #After loop, write out summary stuff
 print('Financial Analysis')
 print('--------------------')
-
+print("Total Months: " + str(total_months))
+print("Total: " + "$" + str(total_profit_losses))
+print("Average Change: " + "$" + str(int(sum_profit_losses / (total_months - 1))))
+print("Greatest Increase in Profits: " + )
 
 # Print other lines
 # Print out total_profit_losses
--- README.md
+++ README.md
@@ -1,5 +1,59 @@
 # python-challenge
+# PSUEDO TEXT GIVEN FROM BILL
+import os
+
+
+# Read in all the rows (looping through the rows)
+
+
+total_profit_losses = 0
+total_months = 0
+
+sum_profit_loses = 0
 
 
 
+# Need profit/losses in the for loop
 
+    profit_loses = row[1]
+
+    total_profit_losses = total_profit_losses + profit_loses
+
+    total_months = total_months + 1
+
+    if total_months > 1:
+        change = profit_loses - last_profit_losses
+
+    last_profit_loses = row[1]
+
+#After loop, write out summary stuff
+print('Financial Analysis')
+print('--------------------')
+
+
+# Print other lines
+# Print out total_profit_losses
+#Print out average of profit losses, which is sum_profit_losses / (total_months - 1)
+
+
+
+------------------------------------------------------------------------------------------
+
+DAY 2 ACTIVITY 8 - LOOPING THROUGH ROWS OF CSV FILE:
+
+# Method 2: Improved Reading using CSV module
+
+with open(csvpath) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    # Read the header row first (skip this step if there is no header)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    # Read each row of data after the header
+    for row in csvreader:
+        print(row)
